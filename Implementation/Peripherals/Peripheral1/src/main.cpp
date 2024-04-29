9ba0d5b80b4b35335308b9d6eff1709f34114e99
BatMitio <mitko_zlia@abv.bg>
2024-04-30 00:32:55 +0200

    Added initial Peripherals code to interface with the display and also added the initial simulation

--- Implementation/Peripherals/Peripheral1/src/main.cpp
+++ Implementation/Peripherals/Peripheral1/src/main.cpp
@@ -1,7 +1,86 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-void setup() {
+LiquidCrystal_I2C lcd(0x27, 20, 4); // LCD Address, row, cols
+
+enum ReadMode
+{
+  COMMAND,
+  PROCESSING,
+  PROCESSING_BULK
+};
+
+char serialBuffer[256];
+int serialBufferIndex = 0;
+ReadMode serialCommandMode = COMMAND;
+void (*serialProcessor)();
+int serialBytesToRead = 0;
+
+byte pixelR, pixelG, pixelB;
+
+void setup()
+{
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  Serial.begin(9600);
 }
 
-void loop() {
+// Put character from serial in the buffer if available and return true if CRLF detected (Command mode) of if in Bulk read mode
+bool serialRead()
+{
+  if (Serial.available())
+  {
+    serialBuffer[serialBufferIndex] = Serial.read();
+    serialBufferIndex++;
+    if (serialBufferIndex > 1 && serialBuffer[serialBufferIndex - 2] == '\r' && serialBuffer[serialBufferIndex - 1] == '\n')
+    {
+      serialBuffer[serialBufferIndex - 2] = '\0';
+      return true;
+    }
+  }
+  return false;
+}
+
+void writeToDisplay(String text)
+{
+  lcd.clear();
+  lcd.print(text.c_str());
+}
+
+// Handle collected serial commands from the buffer.
+// We define a command name from the first three
+// letters in the command, any consequent letters represent parameters.
+void handleSerial()
+{
+  // Serial.println(serialBuffer);
+  String command = String(serialBuffer);
+  // Serial.println(command);
+  if (command.substring(0, 3) == "WTD")
+  {
+    writeToDisplay(command.substring(3));
+    Serial.println(command.substring(3));
+  }
+  else if (command.substring(0, 3) == "BLK")
+  {
+    
+  }
+  serialBufferIndex = 0;
+}
+
+void loop()
+{
+  if (serialRead())
+  {
+    handleSerial();
+  }
+
+  // pixels.setPixelColor(0, pixels.Color(pixelR, pixelG, pixelB));
+
+  // if (Serial.available()) {
+  //   lcd.clear();
+  //   while (Serial.available() > 0) {
+  //     lcd.write(Serial.read());
+  //   }
+  // }
 }
---
+++ Simulation/LuggageSimulator/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public List<GameObject> onBelt;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        foreach (var item in onBelt)
+        {
+            item.GetComponent<Rigidbody>().velocity = direction * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
+    }
+}
